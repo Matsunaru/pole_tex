2872ce21b136aa970131ea94f19c28acc025c636
Matsunaru <error1998pl@gmail.com>
2024-04-16 21:43:18 +0200

    almost there

--- text.cpp
+++ text.cpp
@@ -10,12 +10,14 @@ using namespace std;
 
 HANDLE console;
 
-const int TEXT_BOX_WIDTH = 10;
+const int TEXT_BOX_WIDTH = 11;
 const int TEXT_BOX_HEIGHT = 1;
-const int START_X = 10;
-const int START_Y = 20;
+const int START_X = 30;
+const int START_Y = 10;
 
 int cursorPosition = 0;
+char textBox[TEXT_BOX_WIDTH + 1] = {};
+WORD textAttributes[TEXT_BOX_WIDTH] = {};
 
 void Draw() {
     COORD pos;
@@ -46,11 +48,48 @@ void MoveCursorRight() {
     }
 }
 
+
 void DrawCursor() {
     COORD cursorPos;
     cursorPos.X = START_X + cursorPosition;
     cursorPos.Y = START_Y;
     SetConsoleCursorPosition(console, cursorPos);
+}
+
+void InsertChar(char key) {
+    if (cursorPosition < TEXT_BOX_WIDTH - 1) {
+        textBox[cursorPosition] = key;
+        textAttributes[cursorPosition] = FOREGROUND_RED | BACKGROUND_BLUE;
+        cursorPosition++;
+    }
+}
+
+
+
+void RefreshTextBox() {
+    COORD pos;
+    pos.X = START_X;
+    pos.Y = START_Y;
+    SetConsoleCursorPosition(console, pos);
+    for (int i = 0; i < TEXT_BOX_WIDTH; ++i) {
+        SetConsoleTextAttribute(console, textAttributes[i]);
+        std::cout << textBox[i];
+    }
+    SetConsoleTextAttribute(console, 0);
+}
+
+void DeleteChar() {
+    if (cursorPosition > 0) {
+        std::move(textBox + cursorPosition, textBox + TEXT_BOX_WIDTH, textBox + cursorPosition - 1);
+        std::move(textAttributes + cursorPosition, textAttributes + TEXT_BOX_WIDTH, textAttributes + cursorPosition - 1);
+
+        textBox[TEXT_BOX_WIDTH - 1] = ' ';
+        textAttributes[TEXT_BOX_WIDTH - 1] = BACKGROUND_BLUE;
+
+        cursorPosition--;
+
+        RefreshTextBox();
+    }
 }
 
 int main()
@@ -59,25 +98,28 @@ int main()
     Draw();
     DrawCursor();
     while (true) {
-    if (_kbhit()) {
-        char key = _getch();
-        if (key == 75) { 
-            MoveCursorLeft();
+        if (_kbhit()) {
+            char key = _getch();
+            if (key == 75) {
+                MoveCursorLeft();
+            }
+            else if (key == 77) {
+                MoveCursorRight();
+            }
+            else if ((key >= 'a' && key <= 'z') || (key >= 'A' && key <= 'Z')) {
+                InsertChar(key);
+                RefreshTextBox();
+            }
+            else if (key == 8) {
+                DeleteChar();
+                
+
+            }
+
+
+            DrawCursor();
         }
-        else if (key == 77) { 
-            MoveCursorRight();
-        }
-        else if ((key >= 'a' && key <= 'z') || (key >= 'A' && key <= 'Z')) {
-            COORD pos;
-            pos.X = START_X + cursorPosition;
-            pos.Y = START_Y;
-            SetConsoleTextAttribute(console, FOREGROUND_RED);
-            std::cout << key;
-            MoveCursorRight();
-        }
-        DrawCursor(); 
     }
-}
-    
+
     return 0;
 }
